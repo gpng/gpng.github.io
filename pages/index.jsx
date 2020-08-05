78fd9c4bacc1094cbd333ad70784319bbf4a5a73
Gerald <geraldpng@gmail.com>
2020-08-06 03:15:06 +0800

    initial commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-// translations
-import useTranslations from '../translations/useTranslations';
 
 const Index = () => {
-  const { t } = useTranslations();
-
   return (
     <div className="root">
-      <h1>{t('index.title')}</h1>
+      <h1>gpng.github.io</h1>
       <style jsx>
         {`
           .root {
