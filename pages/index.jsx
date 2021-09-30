317f532425d4801505698adbf6d048c12bcc7777
Gerald <geraldpng@gmail.com>
2021-09-30 18:11:45 +0800

    Add contact

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,10 +11,28 @@ const Index = () => {
       </Head>
       <div className="root">
         <h1>Gerald Png</h1>
+        <div className="links">
+          <a href="https://github.com/gpng" target="_blank" rel="noopener noreferrer">
+            <img src="/static/icons/github.svg" alt="Github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/geraldpng/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/static/icons/linkedin.svg" alt="LinkedIn" />
+          </a>
+          <a href="mailto:geraldpng+hello@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src="/static/icons/gmail.svg" alt="gmail" />
+          </a>
+        </div>
         <h2>About Me</h2>
         <ul>
           <li>Based in Singapore</li>
-          <li>Co-founder / Lead developer of <a href="https://greatescape.co">greatescape.co</a> and <a href="https://covidcontrols.co">covidcontrols.co</a></li>
+          <li>
+            Co-founder / Lead developer of <a href="https://greatescape.co">greatescape.co</a> and{' '}
+            <a href="https://covidcontrols.co">covidcontrols.co</a>
+          </li>
         </ul>
         <h2>What I do</h2>
         <ul>
@@ -34,7 +52,7 @@ const Index = () => {
             h1 {
               font-size: 3rem;
               font-weight: bold;
-              margin: 0 0 2rem;
+              margin: 0 0 0.5rem;
             }
 
             h2 {
@@ -44,7 +62,20 @@ const Index = () => {
             }
 
             ul {
-              font-size: 1rem
+              font-size: 1rem;
+            }
+
+            .links {
+              display: flex;
+              margin: 0 0 1rem;
+            }
+
+            .links > a + a {
+              margin-left: 1rem;
+            }
+
+            .links > a > img {
+              width: 1.5rem;
             }
 
             @media only screen and (min-width: 600px) {
@@ -52,19 +83,23 @@ const Index = () => {
                 padding: 6rem 2rem;
               }
 
-            h1 {
-              font-size: 4rem;
-              margin: 0 0 3rem;
-            }
+              h1 {
+                font-size: 4rem;
+                margin: 0 0 1rem;
+              }
 
-            h2 {
-              font-size: 3rem;
-              margin: 0 0 2rem;
-            }
+              h2 {
+                font-size: 3rem;
+                margin: 0 0 2rem;
+              }
 
-            ul {
-              font-size: 1.5rem
-            }
+              ul {
+                font-size: 1.5rem;
+              }
+
+              .links {
+                margin: 0 0 2rem;
+              }
             }
           `}
         </style>
