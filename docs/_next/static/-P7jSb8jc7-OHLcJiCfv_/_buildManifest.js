f1c9dcca83fe97fe57290f65c6fbf72aba970e2b
Gerald <geraldpng@gmail.com>
2022-08-02 18:46:07 +0800

    Export

---
+++ docs/_next/static/-P7jSb8jc7-OHLcJiCfv_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/421-cc717c56749450f0.js","static/chunks/pages/index-3e1928cf3aa3ef75.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- docs/_next/static/79a3SHEl8QK9hw2zJoV4Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/421-cc717c56749450f0.js","static/chunks/pages/index-5b50f1b2a5663699.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
